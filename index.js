d2613d195a122e0a99cec5b4146fc6bd502674c7
Maxi-arch <mxmu025@gmail.com>
2024-09-18 17:12:38 +0300

    objects

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee ={
+    name:"John",
+    streetAddress:"10th downing street",
+
+};
+function updateEmployeeWithKeyAndValue(object,key,value) {
+      const newemp ={...employee};
+      newemp[key] ="11 Broadway";
+      
+      return newemp;
+
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(object,streetAddress,value){
+    employee[streetAddress] ="12 Broadway"
+    
+    return employee;
+}
+function deleteFromEmployeeByKey(sam,streetAddress,value){
+    const newiish ={ ...employee};
+    delete newiish.name;
+    
+    return newiish;
+}
+function destructivelyDeleteFromEmployeeByKey (object,streetAddress,value) {
+    delete employee.name
+    return employee
+    
+}
